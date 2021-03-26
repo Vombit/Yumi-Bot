15fa2a76fc415855a0cc0a3565fa1393c07f8439
Vombit <45901675+Vombit@users.noreply.github.com>
2021-03-26 08:50:36 +0500

    Update music_loop.js

--- cmd/music_loop.js
+++ cmd/music_loop.js
@@ -7,7 +7,6 @@ module.exports = {
 	inVoiceChannel: true
 };
 module.exports.run = async (bot, message, args) => {
-	if (message.guild.id != '400355776717127690')	return message.channel.send('You don\'t have a premium account');
 	if(message.guild.region === 'russia') lang = rus;
 	if(message.guild.region != 'russia') lang = eng;
 
