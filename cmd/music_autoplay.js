a1609d592ddbb4d483903a789f6a431cc5cc4bcc
Vombit <45901675+Vombit@users.noreply.github.com>
2021-03-26 08:42:53 +0500

    Update music_autoplay.js

--- cmd/music_autoplay.js
+++ cmd/music_autoplay.js
@@ -8,8 +8,7 @@ module.exports = {
 	inVoiceChannel: true
 };
 module.exports.run = async (bot, message, args) => {
-	if (message.guild.id != '400355776717127690')	return message.channel.send('We are sorry, command is not working correctly and we are trying to fix it');
-	//if(message.author.id != '317598066276565003') return message.channel.send('We are sorry, command is not working correctly and we are trying to fix it');
+	if (message.guild.id != '400355776717127690')	return message.channel.send('You don\'t have a premium account');
 	if(message.guild.region === 'russia') lang = rus;
 	if(message.guild.region != 'russia') lang = eng;
 
